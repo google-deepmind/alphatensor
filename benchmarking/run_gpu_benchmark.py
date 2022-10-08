6f9d3656ef3956661d2643b7567cd8a66dd0fe12
vcarpani <v.carpani@fizyr.com>
2022-10-08 15:37:07 +0200

    Allow the usage of custom GPUs for the benchmark.

--- benchmarking/run_gpu_benchmark.py
+++ benchmarking/run_gpu_benchmark.py
@@ -37,6 +37,8 @@ pip install --upgrade "jax[cuda]" \
 ```
 """
 import subprocess
+import sys
+from   argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 import numpy as np
 # Might be needed on GCP because of the following bug:
@@ -46,25 +48,91 @@ import scipy.signal  # pylint: disable=unused-import
 from alphatensor.benchmarking import factorizations
 from alphatensor.benchmarking import utils
 
+def parse_args(arg_list = sys.argv[1:]):
+  parser = ArgumentParser(
+    description='Script for benchmarking the AlphaTensor paper.',
+    formatter_class=ArgumentDefaultsHelpFormatter,
+  )
 
-def main():
-  process = subprocess.Popen(['nvidia-smi'], stdout=subprocess.PIPE)
-  output, _ = process.communicate()
-  if 'V100' not in str(output):
-    raise ValueError('To reproduce the results from the paper, please run on a'
-                     'server with V100 GPU.')
-  print('Fixing GPU clock frequency to 1530 to reduce benchmarking variance...')
-  process = subprocess.Popen(
-      'sudo nvidia-smi -pm ENABLED -i 0'.split(' '), stdout=subprocess.PIPE)
-  output, _ = process.communicate()
-  process = subprocess.Popen(
-      'sudo nvidia-smi --lock-gpu-clocks=1530,1530'.split(' '),
-      stdout=subprocess.PIPE)
-  output, _ = process.communicate()
-  print('Done.')
+  parser.add_argument(
+    '--custom-gpu',
+    action='store_true',
+    help="Use a GPU that is not the V100 used for the original paper."
+  )
+  parser.add_argument(
+    '--gpu-clock',
+    type=int,
+    default=1530,
+    help="Fixed GPU clock to reduce benchmarking variance."
+  )
+  parser.add_argument(
+    '--matrix-sizes',
+    choices=['8192', '10240', '12288', '14336', '16384', '18432', '20480'],
+    action='append',
+    help="Matrix sizes to be benchmarked, default is all of the choices."
+  )
+
+  args, unknown = parser.parse_known_args(arg_list)
+
+  if len(unknown) > 0:
+    print(f"The following command line arguments were not recognized: {unknown}.")
+  return args
+
+def main(args=None):
+  if args is None:
+        args = parse_args()
+
+  # Reproduce the original results of the AlphaTensor paper on a V100 GPU.
+  if not args.custom_gpu:
+    # Check if the GPU is a V100.
+    process = subprocess.Popen(['nvidia-smi'], stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+    if 'V100' not in str(output):
+      raise ValueError('To reproduce the results from the paper, please run on a'
+                       ' server with V100 GPU.')
+
+    # Make sure that the benchmarking variance is low by fixing clocks.
+    print(f'Fixing GPU clock frequency to {args.gpu_clock} to reduce benchmarking variance...')
+    process = subprocess.Popen(
+        'sudo nvidia-smi -pm ENABLED -i 0'.split(' '), stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+    process = subprocess.Popen(
+        f'sudo nvidia-smi --lock-gpu-clocks={args.gpu_clock},{args.gpu_clock}'.split(' '),
+        stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+    print('Done.')
+
+  # Benchmark the AlphaTensor paper on different GPUs.
+  else:
+    print('Benchmarking matrix multiplication algorithm discovered by AlphaTensor'
+          ' on a custom GPU. Results may vary, since the algorithm was focused on'
+          ' the V100 GPU.'
+    )
+
+    # Make sure that the benchmarking variance is low by fixing clocks.
+    process = subprocess.Popen(
+        'sudo nvidia-smi -pm ENABLED -i 0'.split(' '), stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+    if 'All done' not in str(output):
+      print('Failed to enable persistent mode, this would increase benchmarking'
+            ' variance.'
+      )
+    process = subprocess.Popen(
+        f'sudo nvidia-smi --lock-gpu-clocks={args.gpu_clock},{args.gpu_clock}'.split(' '),
+        stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+    if 'All done' not in str(output) or 'not supported' in str(output):
+      print('Failed to fix clock of the GPU, this would increase benchmarking'
+            ' variance.'
+      )
+    print('Done.')
 
   num_trials = 10
-  matrix_sizes = [8192, 10240, 12288, 14336, 16384, 18432, 20480]
+
+  if (args.matrix_sizes):
+    matrix_sizes = list(map(int, args.matrix_sizes))
+  else:
+    matrix_sizes = [8192, 10240, 12288, 14336, 16384, 18432, 20480]
 
   factorized_algorithms = [
       ('Strassen^2', factorizations.get_4x4x4_strassen_squared()),
@@ -90,4 +158,4 @@ def main():
 
 
 if __name__ == '__main__':
-  main()
+  main(parse_args())
